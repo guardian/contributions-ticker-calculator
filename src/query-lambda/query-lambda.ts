b614985db7defc91b22fd1dfdc180472e7424db0
Tom Forbes <tom.forbes@theguardian.com>
2018-12-11 11:24:31 +0000

    Use state machine (#3)
    * Split into 2 lambdas (cloudformation)
    
    * Split into 2 lambdas (code)
    
    * shorter pending message
    
    * debug
    
    * step function
    
    * fix path
    
    * return just the ids

---
+++ src/query-lambda/query-lambda.ts
@@ -0,0 +1,40 @@
+import {Config} from '../config';
+import {StartQueryExecutionInput, StartQueryExecutionOutput} from "aws-sdk/clients/athena";
+import moment = require("moment");
+import {Moment} from "moment";
+import {getQueries, Query} from "./queries";
+
+const AWS = require('aws-sdk');
+const config = new Config();
+const athena = new AWS.Athena({region: 'eu-west-1'});
+
+/**
+ * Executes Athena queries and returns the execution IDs
+ */
+export async function handler(): Promise<string[]> {
+
+    const startDateTime: Moment = moment(config.StartDateTime);
+    const endDateTime: Moment = moment(config.EndDateTime);
+
+    console.log(`Getting total for period ${config.StartDateTime}-${config.EndDateTime} and country codes ${config.CountryCodesString}`);
+
+    const queries = getQueries(startDateTime, endDateTime, config.CountryCodesString);
+
+    return Promise.all(queries.map(executeQuery))
+        .then((results: StartQueryExecutionOutput[]) => results.map(result => result.QueryExecutionId))
+}
+
+function executeQuery(query: Query): Promise<StartQueryExecutionOutput> {
+    const params: StartQueryExecutionInput = {
+        QueryString: query.query,
+        ResultConfiguration: {
+            OutputLocation: `s3://${config.AthenaOutputBucket}`,
+        },
+        ClientRequestToken: query.token,
+        QueryExecutionContext: {
+            Database: config.SchemaName
+        }
+    };
+
+    return athena.startQueryExecution(params).promise()
+}
