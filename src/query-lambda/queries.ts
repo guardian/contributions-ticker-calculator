0dc55a19ed88cef355f35f90d947a80f135f04ad
Tom Forbes <tom.forbes@theguardian.com>
2018-12-19 12:11:26 +0000

    One query (#10)
    * only use 1 query in final month
    
    * rename dateTime to date
    
    * correct name

--- src/query-lambda/queries.ts
+++ src/query-lambda/queries.ts
@@ -10,35 +10,37 @@ export class Query {
     }
 }
 
-const oneOffAndAnnualQuery = (startDateTime: Moment, countryCodesString: string, tableName: string) => new Query(
+const formatDateTime = (dt: Moment) => dt.format('YYYY-MM-DD ')
+
+const fullQuery = (startDate: Moment, countryCodesString: string, tableName: string) => new Query(
     'SELECT SUM(amount) ' +
         `FROM ${tableName} ` +
         `WHERE country_code in (${countryCodesString}) ` +
-        `AND timestamp > CAST('${startDateTime.format('YYYY-MM-DD')}' AS TIMESTAMP) ` +
+        `AND timestamp > CAST('${startDate.format('YYYY-MM-DD')}' AS TIMESTAMP) ` +
+        `AND payment_frequency IN ('OneOff', 'Annual', 'Monthly')`,
+    'acquisition_events_full'
+);
+
+const oneOffAndAnnualQuery = (startDate: Moment, countryCodesString: string, tableName: string) => new Query(
+    'SELECT SUM(amount) ' +
+        `FROM ${tableName} ` +
+        `WHERE country_code in (${countryCodesString}) ` +
+        `AND timestamp > CAST('${startDate.format('YYYY-MM-DD')}' AS TIMESTAMP) ` +
         `AND payment_frequency IN ('OneOff', 'Annual')`,
     'acquisition_events_oneOffAndAnnual'
 );
 
-const fullMonthlyQuery = (startDateTime: Moment, countryCodesString: string, tableName: string) => new Query(
-    'SELECT SUM(amount) ' +
-        `FROM ${tableName} ` +
-        `WHERE country_code in (${countryCodesString}) ` +
-        `AND timestamp > CAST('${startDateTime.format('YYYY-MM-DD')}' AS TIMESTAMP) ` +
-        `AND payment_frequency='Monthly'`,
-    'acquisition_events_fullMonthlyQuery'
-);
-
-const firstMonthlyQuery = (startDateTime: Moment, oneMonthBeforeEnd: Moment, countryCodesString: string, tableName: string) => new Query(
+const firstMonthlyQuery = (startDate: Moment, oneMonthBeforeEnd: Moment, countryCodesString: string, tableName: string) => new Query(
     'SELECT SUM(amount)*2 ' +
         `FROM ${tableName} ` +
         `WHERE country_code in (${countryCodesString}) ` +
-        `AND timestamp > CAST('${startDateTime.format('YYYY-MM-DD')}' AS TIMESTAMP) ` +
+        `AND timestamp > CAST('${startDate.format('YYYY-MM-DD')}' AS TIMESTAMP) ` +
         `AND timestamp < CAST('${oneMonthBeforeEnd.format('YYYY-MM-DD')}' AS TIMESTAMP) ` +
         `AND payment_frequency='Monthly'`,
     'acquisition_events_firstMonthlyQuery'
 );
 
-const secondMonthlyQuery = (endDateTime: Moment, oneMonthBeforeEnd: Moment, countryCodesString: string, tableName: string) => new Query(
+const secondMonthlyQuery = (endDate: Moment, oneMonthBeforeEnd: Moment, countryCodesString: string, tableName: string) => new Query(
     'SELECT SUM(amount) ' +
         `FROM ${tableName} ` +
         `WHERE country_code in (${countryCodesString}) ` +
@@ -51,17 +53,14 @@ const secondMonthlyQuery = (endDateTime: Moment, oneMonthBeforeEnd: Moment, coun
  * If a campaign runs for more than a month then double any monthly contributions received before the final month.
  * This logic assumes campaigns will not run for more than 2 months.
  */
-export function getQueries(startDateTime: Moment, endDateTime: Moment, countryCodesString: string, stage: string): Query[] {
-    const oneMonthBeforeEnd = endDateTime.clone().subtract(1, 'month');
+export function getQueries(startDate: Moment, endDate: Moment, countryCodesString: string, stage: string): Query[] {
+    const oneMonthBeforeEnd = endDate.clone().subtract(1, 'month');
     const tableName = `acquisition_events_${stage.toLowerCase()}`;
 
-    if (oneMonthBeforeEnd.isAfter(startDateTime)) return [
-        oneOffAndAnnualQuery(startDateTime, countryCodesString, tableName),
-        firstMonthlyQuery(startDateTime, oneMonthBeforeEnd, countryCodesString, tableName),
-        secondMonthlyQuery(endDateTime, oneMonthBeforeEnd, countryCodesString, tableName)
+    if (oneMonthBeforeEnd.isAfter(startDate)) return [
+        oneOffAndAnnualQuery(startDate, countryCodesString, tableName),
+        firstMonthlyQuery(startDate, oneMonthBeforeEnd, countryCodesString, tableName),
+        secondMonthlyQuery(endDate, oneMonthBeforeEnd, countryCodesString, tableName)
     ];
-    else return [
-        oneOffAndAnnualQuery(startDateTime, countryCodesString, tableName),
-        fullMonthlyQuery(startDateTime, countryCodesString, tableName)
-    ];
+    else return [fullQuery(startDate, countryCodesString, tableName)];
 }
--- src/query-lambda/query-lambda.ts
+++ src/query-lambda/query-lambda.ts
@@ -6,8 +6,8 @@ import {getQueries, Query} from "./queries";
 class Config {
     Stage: string = process.env.Stage;
 
-    StartDateTime: string = process.env.StartDateTime;
-    EndDateTime: string = process.env.EndDateTime;
+    StartDate: string = process.env.StartDate;
+    EndDate: string = process.env.EndDate;
 
     //A quoted, comma-separated list as a string, e.g. 'US','AU'
     CountryCodesString: string = process.env.CountryCodes;
@@ -26,12 +26,12 @@ const athena = new AWS.Athena({region: 'eu-west-1'});
  */
 export async function handler(): Promise<string[]> {
 
-    const startDateTime: Moment = moment(config.StartDateTime);
-    const endDateTime: Moment = moment(config.EndDateTime);
+    const StartDate: Moment = moment(config.StartDate);
+    const EndDate: Moment = moment(config.EndDate);
 
-    console.log(`Getting total for period ${config.StartDateTime}-${config.EndDateTime} and country codes ${config.CountryCodesString}`);
+    console.log(`Getting total for period ${config.StartDate}-${config.EndDate} and country codes ${config.CountryCodesString}`);
 
-    const queries = getQueries(startDateTime, endDateTime, config.CountryCodesString, config.Stage);
+    const queries = getQueries(StartDate, EndDate, config.CountryCodesString, config.Stage);
 
     return Promise.all(queries.map(executeQuery))
         .then((results: StartQueryExecutionOutput[]) => results.map(result => result.QueryExecutionId))
