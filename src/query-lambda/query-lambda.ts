71c918991fbb0c14553c69b2c0ced51a1e0f6d3b
Tom Forbes <tom.forbes@theguardian.com>
2019-03-12 13:02:38 +0000

    Query by campaignCode (#20)
    * Filter by componentId
    
    * Use campaignCode

--- src/query-lambda/query-lambda.ts
+++ src/query-lambda/query-lambda.ts
@@ -11,6 +11,7 @@ class Config {
 
     CountryCode: string = process.env.CountryCode;
     Currency: string = process.env.Currency;
+    CampaignCode: string = process.env.CampaignCode;
 
     AthenaOutputBucket: string = process.env.AthenaOutputBucket;
 
@@ -31,7 +32,7 @@ export async function handler(): Promise<string[]> {
 
     console.log(`Getting total for period ${config.StartDate}-${config.EndDate} with country code ${config.CountryCode} and currency ${config.Currency}`);
 
-    const queries = getQueries(StartDate, EndDate, config.CountryCode, config.Currency, config.Stage);
+    const queries = getQueries(StartDate, EndDate, config.CountryCode, config.Currency, config.Stage, config.CampaignCode);
 
     return Promise.all(queries.map(executeQuery))
         .then((results: StartQueryExecutionOutput[]) => results.map(result => result.QueryExecutionId))
