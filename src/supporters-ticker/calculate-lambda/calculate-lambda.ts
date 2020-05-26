f69814fabb7f8f1cd1eec477377c72303dc27a69
Tom Forbes <tom.forbes@theguardian.com>
2020-05-26 10:30:36 +0100

    New state machine for counting supporters

---
+++ src/supporters-ticker/calculate-lambda/calculate-lambda.ts
@@ -0,0 +1,46 @@
+import {ManagedUpload} from "aws-sdk/lib/s3/managed_upload";
+import {
+    GetQueryResultsOutput,
+    QueryExecutionId,
+} from "aws-sdk/clients/athena";
+import {QueryReduce, reduceAndWrite} from "../../lib/process";
+
+class Config {
+    Stage: string = process.env.Stage;
+
+    ExtraSupporters: number = parseInt(process.env.ExtraSupporters);
+    GoalAmount: number = parseInt(process.env.GoalAmount);
+
+    TickerBucket: string = process.env.TickerBucket;
+}
+
+const config = new Config();
+
+export async function handler(executionIds: QueryExecutionId[]): Promise<ManagedUpload.SendData> {
+    return reduceAndWrite(
+        executionIds,
+        reduce,
+        config.TickerBucket,
+        `${config.Stage}/ticker.json`
+    );
+}
+
+const reduce: QueryReduce = (queryResults: GetQueryResultsOutput[]) => {
+    const amounts: number[] = queryResults.map((result: GetQueryResultsOutput) => {
+        const value = parseFloat(result.ResultSet.Rows[1].Data[0].VarCharValue);
+        if (value) return value;
+        else {
+            //This will happen if there are no results for this particular query
+            console.log(`No results for query: ${JSON.stringify(result)}`);
+            return 0
+        }
+    });
+
+    const total = amounts.reduce((sum, v) => sum + v) + config.ExtraSupporters;
+
+    // TODO - include breakdown by state
+    return {
+        total,
+        goal: config.GoalAmount
+    };
+};
---
+++ src/supporters-ticker/query-lambda/query-lambda.ts
@@ -0,0 +1,37 @@
+import {QueryExecutionId} from "aws-sdk/clients/athena";
+import {executeQueries, Query} from "../../lib/query";
+
+class Config {
+    AthenaOutputBucket: string = process.env.AthenaOutputBucket;
+    SchemaName: string = process.env.SchemaName;
+}
+
+const config = new Config();
+
+const subscriptionsQuery =
+    "SELECT\n" +
+    "  COUNT(DISTINCT identity_id)\n" +
+    "  FROM clean.subscriptions\n" +
+    "WHERE\n" +
+    "  sold_to_country = 'Australia'\n" +
+    "  AND status = 'active'\n" +
+    "  AND (product_name IN ('Supporter','Partner','Patron','Contributor','Newspaper Delivery','Newspaper Voucher','Digital Pack') or product_name LIKE '%Weekly%')";
+
+const singleContributionsQuery =
+    "SELECT \n" +
+    "  COUNT(DISTINCT browser_id) AS cons\n" +
+    "  FROM clean.acquisitions_no_pageview_data\n" +
+    "WHERE\n" +
+    "  received_date >= DATE '2019-05-01'\n" +
+    "  AND country_code = 'AU'\n" +
+    "  AND product = 'CONTRIBUTION'";
+
+export async function handler(): Promise<QueryExecutionId[]> {
+    // TODO - query for breakdown by state
+    const queries = [
+        new Query(subscriptionsQuery, 'aus_supporters__subscriptions'),
+        new Query(singleContributionsQuery, 'aus_supporters__single_contributions')
+    ];
+
+    return executeQueries(queries, config.AthenaOutputBucket, config.SchemaName);
+}
