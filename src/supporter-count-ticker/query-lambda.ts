ff4b7a46868f695b5fdfe43966723b578718a580
Tom Forbes <tom.forbes@theguardian.com>
2020-06-01 14:01:37 +0100

    tidy sql

--- src/supporter-count-ticker/query-lambda.ts
+++ src/supporter-count-ticker/query-lambda.ts
@@ -9,21 +9,21 @@ class Config {
 const config = new Config();
 
 const subscriptionsQuery =
-    "SELECT" +
-    "  COUNT(DISTINCT identity_id)" +
-    "  FROM clean.subscriptions" +
-    "WHERE" +
-    "  sold_to_country = 'Australia'" +
-    "  AND status = 'active'" +
+    "SELECT " +
+    "  COUNT(DISTINCT identity_id) " +
+    "  FROM clean.subscriptions " +
+    "WHERE " +
+    "  sold_to_country = 'Australia' " +
+    "  AND status = 'active' " +
     "  AND (product_name IN ('Supporter','Partner','Patron','Contributor','Newspaper Delivery','Newspaper Voucher','Digital Pack') or product_name LIKE '%Weekly%')";
 
 const singleContributionsQuery =
-    "SELECT" +
-    "  COUNT(DISTINCT browser_id) AS cons" +
-    "  FROM clean.acquisitions_no_pageview_data" +
-    "WHERE" +
-    "  received_date >= DATE '2019-05-01'" +
-    "  AND country_code = 'AU'" +
+    "SELECT " +
+    "  COUNT(DISTINCT browser_id) AS cons " +
+    "  FROM clean.acquisitions_no_pageview_data " +
+    "WHERE " +
+    "  received_date >= DATE '2019-05-01' " +
+    "  AND country_code = 'AU' " +
     "  AND product = 'CONTRIBUTION'";
 
 export async function handler(): Promise<QueryExecutionId[]> {
