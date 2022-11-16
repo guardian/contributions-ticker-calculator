6fa25d18b6f759553db2ce3dd6210b225580751b
Tom Forbes <tom.forbes@theguardian.com>
2022-11-16 16:47:46 +0000

    no local

--- src/value-ticker/calculate-lambda/local.ts
+++ src/value-ticker/calculate-lambda/local.ts
@@ -12,10 +12,10 @@ AWS.config.secretAccessKey = process.env.AWS_SECRET_ACCESS_KEY;
 AWS.config.sessionToken = process.env.AWS_SESSION_TOKEN;
 AWS.config.region = "eu-west-1";
 
-async function run(ids: string) {
-    await handler(ids.split(','))
-        .then(result => console.log(`Result: ${JSON.stringify(result)}`))
-        .catch(err => console.log(`Error: ${err}`))
-}
-
-run(process.argv[2]);
+// async function run(ids: string) {
+//     await handler(ids.split(','))
+//         .then(result => console.log(`Result: ${JSON.stringify(result)}`))
+//         .catch(err => console.log(`Error: ${err}`))
+// }
+//
+// run(process.argv[2]);
