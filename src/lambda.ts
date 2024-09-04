58084332d1b63f3462e15bda9f1671b50b1b7479
Tom Forbes <tom.forbes@theguardian.com>
2024-09-04 15:03:46 +0100

    Reimplement as single lambda with cdk (#68)
    * Reimplement as single lambda
    
    * set input
    
    * log event
    
    * update readme
    
    * fix build path
    
    * event type
    
    * fix stack ids
    
    * event type
    
    * update local
    
    * prepend stack
    
    * remove switch
    
    * event as string
    
    * log format
    
    * log campaignName
    
    * update readme

---
+++ src/lambda.ts
@@ -0,0 +1,27 @@
+import { getSSMParam } from "./lib/ssm";
+
+interface Config {
+    StartDate: string;
+    EndDate: string;
+    CountryCode: string;
+    Currency: string;
+    CampaignCode?: string;
+}
+
+export async function handler(campaignName: string): Promise<void> {
+    console.log('campaignName: ', campaignName);
+    const stage = process.env.Stage;
+    if (!stage || (stage !== 'CODE' && stage !== 'PROD')) {
+        return Promise.reject(`Invalid or missing stage: '${stage ?? ''}'`);
+    }
+
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment -- config
+    const tickerConfig: Record<string,Config> = JSON.parse(await getSSMParam('ticker-config', stage));
+    const gcpConfig = await getSSMParam('gcp-wif-credentials-config', stage);
+
+    const campaignConfig = tickerConfig[campaignName];
+    console.log('Using config:', campaignConfig);
+
+    // TODO - implement
+    return Promise.resolve();
+}
--- src/lib/errors.ts
+++
@@ -1,9 +0,0 @@
-export class QueryPendingError extends Error {
-    name = 'QueryPendingError';
-    message = 'Query pending';
-}
-
-export class QueryFailedError extends Error {
-    name = 'QueryFailedError';
-    message = 'Query failed';
-}
