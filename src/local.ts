3d15099bdfa9c45824d0db7e31b9f8cddfc57a2d
Tom Forbes <tom.forbes@theguardian.com>
2024-09-04 13:08:43 +0100

    update local

--- src/local.ts
+++ src/local.ts
@@ -1,5 +1,5 @@
 import { handler } from './lambda';
 
-handler({ 'Name': 'US' })
+handler({message: { 'Name': 'US' }})
     .then(() => console.log('Succeeded!'))
     .catch((e) => console.log('Something went wrong: ', e));
